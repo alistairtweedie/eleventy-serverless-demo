c1ee243b0a8f60ca02c61b1a724991474b6cc863
Alistair <alistair@alistairtweedie.com>
2023-01-24 10:26:44 +0000

    initial test

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,16 +1,19 @@
-module.exports  = {
-  'post-1': {
+module.exports = {
+  '2023-01-01/post-1': {
     slug: 'post-1',
+    date: '2023-01-01',
     title: 'Post 1',
     content: '<p>Content for Post 1</p>',
   },
-  'post-2': {
+  '2023-01-02/post-2': {
     slug: 'post-2',
+    date: '2023-01-02',
     title: 'Post 2',
     content: '<p>Content for Post 2</p>',
   },
-  'post-3': {
+  '2023-01-03/post-3': {
     slug: 'post-3',
+    date: '2023-01-03',
     title: 'Post 3',
     content: '<p>Content for Post 3</p>',
   },
